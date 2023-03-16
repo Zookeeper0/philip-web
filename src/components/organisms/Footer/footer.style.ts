371172da83f23ca2a596001b7f7f64fc31a90016
hanahreum <hanahreumh@gmail.com>
2023-03-16 13:11:31 +0900

    하단 푸터 문의안내 리스트 추가

--- src/components/organisms/Footer/footer.style.ts
+++ src/components/organisms/Footer/footer.style.ts
@@ -17,10 +17,37 @@ export const FooterRow = styled.div`
 
   //모바일 화면 설정
   @media screen and (max-width: 768px) {
-    padding: 20px 16px 100px;
+    padding: 60px 16px 100px;
     align-items: flex-start;
     flex-direction: column;
     gap: 70px;
+  }
+`;
+
+export const FooterLinkBox = styled.div`
+  display: flex;
+  color: ${(props) => props.theme.colors.white};
+  align-items: center;
+  gap: 80px;
+
+  //모바일 화면 설정
+  @media screen and (max-width: 768px) {
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 60px;
+  }
+`;
+
+export const FooterBtnBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+
+  //모바일 화면 설정
+  @media screen and (max-width: 768px) {
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 20px;
   }
 `;
 
