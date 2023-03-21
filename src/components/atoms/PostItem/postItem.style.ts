e3623913384471fa821c1d9babfda3a9f729a2a1
hanahreum <hanahreumh@gmail.com>
2023-03-21 15:45:02 +0900

    게시글 업체명 임시표기 UI 수정 및 게시글 조회수 아이콘 추가

--- src/components/atoms/PostItem/postItem.style.ts
+++ src/components/atoms/PostItem/postItem.style.ts
@@ -8,26 +8,29 @@ export const PostItem = styled.li`
   cursor: pointer;
   z-index: 0;
 
-  &::before {
-    position: absolute;
-    content: "";
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    opacity: 0;
-    background: ${(props) => props.theme.gradient.dark};
-    transition: all 0.2s ease-in-out;
-    z-index: 1;
-  }
+  // 임시게시글 디자인(사진X)
+  background: ${(props) => props.theme.colors.dark};
 
-  &:hover {
-    box-shadow: ${(props) => props.theme.shadow.dark};
+  // &::before {
+  //   position: absolute;
+  //   content: "";
+  //   top: 0;
+  //   right: 0;
+  //   bottom: 0;
+  //   left: 0;
+  //   opacity: 1;
+  //   background: ${(props) => props.theme.gradient.dark};
+  //   transition: all 0.2s ease-in-out;
+  //   z-index: 1;
+  // }
 
-    &::before {
-      opacity: 1;
-    }
-  }
+  // &:hover {
+  //   box-shadow: ${(props) => props.theme.shadow.dark};
+
+  //   &::before {
+  //     opacity: 1;
+  //   }
+  // }
 
   //모바일 화면 설정
   @media screen and (max-width: 768px) {
@@ -43,30 +46,44 @@ export const PostItemSpan = styled.span`
   right: 0;
   bottom: 16px;
   left: 16px;
-  font-size: 2rem;
-  opacity: 0;
+  // font-size: 2rem;
+  opacity: 1;
   justify-content: flex-end;
   flex-direction: column;
   transition: all 0.2s ease-in-out;
   z-index: 2;
 
-  ${PostItem}:hover & {
-    opacity: 1;
-  }
+  // 임시게시글 디자인(사진X)
+  font-size: 2.6rem;
+  gap: 2px;
+
+  // ${PostItem}:hover & {
+  //   opacity: 1;
+  // }
 
   span {
     color: ${(props) => props.theme.colors.subTxt};
-    font-size: 1.4rem;
+    // font-size: 1.4rem;
+
+    // 임시게시글 디자인(사진X)
+    font-size: 1.8rem;
   }
 
   //모바일 화면 설정
   @media screen and (max-width: 768px) {
-    bottom: 8px;
-    left: 8px;
-    font-size: 1.6rem;
+    // font-size: 1.6rem;
+
+    // 임시게시글 디자인(사진X)
+    left: 0;
+    bottom: 0;
+    align-items: center;
+    justify-content: center;
+    font-size: 2.2rem;
+    gap: 0;
 
     span {
-      font-size: 1.3rem;
+      display: none;
+      // font-size: 1.3rem;
     }
   }
 `;
--- src/components/molecules/StoreInfoBox/index.tsx
+++ src/components/molecules/StoreInfoBox/index.tsx
@@ -2,6 +2,7 @@ import { ImageSlide } from "@/components/atoms/ImageSlide";
 import { CopyButton } from "@/components/atoms/Button/CopyButton";
 import * as S from "./storeInfoBox.style";
 import IconPhone from "public/assets/svg/icon-phone.svg";
+import IconView from "public/assets/svg/icon-view.svg";
 import Images08 from "../../../../public/assets/images/images-4.jpg";
 import { useEffect } from "react";
 import { useMutation } from "react-query";
@@ -13,16 +14,17 @@ export const StoreInfoBox = ({ post, randomImg }: any) => {
       <ImageSlide items={randomImg.images} />
 
       <S.StoreInfo>
-        <S.StoreNameBox>
-          {/* 스토어 네임 */}
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div>{post?.title}</div>
-            <div style={{ fontSize: "16px" }}>
-              방문자수 아이콘 : {post?.views}
-            </div>
-          </div>
-          <S.CategorySpan>{post?.category}</S.CategorySpan>
-        </S.StoreNameBox>
+        <S.StoreInfoTop>
+          <S.StoreNameBox>
+            {/* 스토어 네임 */}
+            {post?.title}
+            <S.CategorySpan>{post?.category}</S.CategorySpan>
+          </S.StoreNameBox>
+          <S.StoreViewBox>
+            <IconView width={16} height={16} viewBox="0 0 24 24" />
+            {post?.views}
+          </S.StoreViewBox>
+        </S.StoreInfoTop>
         <S.AddressBox>{post?.address}</S.AddressBox>
         <S.PhoneBox>
           <span>
