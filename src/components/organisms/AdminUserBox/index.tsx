257667eecb26483f408b0044324a9362a3933986
Shindongju <tlsehdwn5158@gmail.com>
2023-04-20 15:17:33 +0900

    [dongju] search키워드 에러 수정

--- src/components/organisms/AdminUserBox/index.tsx
+++ src/components/organisms/AdminUserBox/index.tsx
@@ -1,12 +1,14 @@
 import { UserGrid } from "@/components/molecules/AdminGrid/UserGrid";
 import { UserSearch } from "@/components/molecules/AdminSearchBox/UserSearch";
 import * as S from "./adminUserBox.style";
+import { useState } from "react";
 
 export const AdminUserBox = () => {
+  const [userSearchKeyword, setUserSearchKeyword] = useState("");
   return (
     <S.AdminUserBox>
-      <UserSearch />
-      <UserGrid />
+      <UserSearch setUserSearchKeyword={setUserSearchKeyword} />
+      <UserGrid userSearchKeyword={userSearchKeyword} />
     </S.AdminUserBox>
   );
 };
